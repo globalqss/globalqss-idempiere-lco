65030be23096687d085441c53c44daf9bf02798a
globalqss <globalqss@localhost>
2010-10-01 16:28:19 +0000

    borrar archivo sobrante

--- compare_with_355.sh
+++
@@ -1,9 +0,0 @@
-BASE=~/srcAdempiere/globalqss/branches/adempiere355a
-for SRC in `find . -name "*.java" | fgrep -v "/X_" | fgrep -v "/I_"`
-do
-    if [ -s $BASE/$SRC ]
-    then
-	echo "********** $SRC ***********"
-        diff -b $SRC $BASE/$SRC
-    fi
-done
