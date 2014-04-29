fd7b329491ae51828caa6756aaa1e4fa3044f519
j2garcia <j2garcia@localhost>
2014-04-29 18:12:40 -0500

    Starting point for LEC Electronic Invoice on 361_maintenance (as extension for 361, to be pluginized on iDempiere)

---
+++ org.globalqss.idempiere.LEC.electronicinvoice/src/org/compiere/model/X_I_SRI_AccessCode.java
@@ -0,0 +1,195 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2007 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+/** Generated Model - DO NOT CHANGE */
+package org.compiere.model;
+
+import java.sql.ResultSet;
+import java.util.Properties;
+
+/** Generated Model for I_SRI_AccessCode
+ *  @author Adempiere (generated) 
+ *  @version 3.6.1LTS.Final - $Id$ */
+public class X_I_SRI_AccessCode extends PO implements I_I_SRI_AccessCode, I_Persistent 
+{
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 20140429L;
+
+    /** Standard Constructor */
+    public X_I_SRI_AccessCode (Properties ctx, int I_SRI_AccessCode_ID, String trxName)
+    {
+      super (ctx, I_SRI_AccessCode_ID, trxName);
+      /** if (I_SRI_AccessCode_ID == 0)
+        {
+			setI_IsImported (false);
+			setI_SRI_AccessCode_ID (0);
+        } */
+    }
+
+    /** Load Constructor */
+    public X_I_SRI_AccessCode (Properties ctx, ResultSet rs, String trxName)
+    {
+      super (ctx, rs, trxName);
+    }
+
+    /** AccessLevel
+      * @return 3 - Client - Org 
+      */
+    protected int get_AccessLevel()
+    {
+      return accessLevel.intValue();
+    }
+
+    /** Load Meta Data */
+    protected POInfo initPO (Properties ctx)
+    {
+      POInfo poi = POInfo.getPOInfo (ctx, Table_ID, get_TrxName());
+      return poi;
+    }
+
+    public String toString()
+    {
+      StringBuffer sb = new StringBuffer ("X_I_SRI_AccessCode[")
+        .append(get_ID()).append("]");
+      return sb.toString();
+    }
+
+	/** Set Import Error Message.
+		@param I_ErrorMsg 
+		Messages generated from import process
+	  */
+	public void setI_ErrorMsg (String I_ErrorMsg)
+	{
+		set_Value (COLUMNNAME_I_ErrorMsg, I_ErrorMsg);
+	}
+
+	/** Get Import Error Message.
+		@return Messages generated from import process
+	  */
+	public String getI_ErrorMsg () 
+	{
+		return (String)get_Value(COLUMNNAME_I_ErrorMsg);
+	}
+
+	/** Set Imported.
+		@param I_IsImported 
+		Has this import been processed
+	  */
+	public void setI_IsImported (boolean I_IsImported)
+	{
+		set_Value (COLUMNNAME_I_IsImported, Boolean.valueOf(I_IsImported));
+	}
+
+	/** Get Imported.
+		@return Has this import been processed
+	  */
+	public boolean isI_IsImported () 
+	{
+		Object oo = get_Value(COLUMNNAME_I_IsImported);
+		if (oo != null) 
+		{
+			 if (oo instanceof Boolean) 
+				 return ((Boolean)oo).booleanValue(); 
+			return "Y".equals(oo);
+		}
+		return false;
+	}
+
+	/** Set I_SRI_AccessCode.
+		@param I_SRI_AccessCode_ID I_SRI_AccessCode	  */
+	public void setI_SRI_AccessCode_ID (int I_SRI_AccessCode_ID)
+	{
+		if (I_SRI_AccessCode_ID < 1) 
+			set_ValueNoCheck (COLUMNNAME_I_SRI_AccessCode_ID, null);
+		else 
+			set_ValueNoCheck (COLUMNNAME_I_SRI_AccessCode_ID, Integer.valueOf(I_SRI_AccessCode_ID));
+	}
+
+	/** Get I_SRI_AccessCode.
+		@return I_SRI_AccessCode	  */
+	public int getI_SRI_AccessCode_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_I_SRI_AccessCode_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set Processed.
+		@param Processed 
+		The document has been processed
+	  */
+	public void setProcessed (boolean Processed)
+	{
+		set_Value (COLUMNNAME_Processed, Boolean.valueOf(Processed));
+	}
+
+	/** Get Processed.
+		@return The document has been processed
+	  */
+	public boolean isProcessed () 
+	{
+		Object oo = get_Value(COLUMNNAME_Processed);
+		if (oo != null) 
+		{
+			 if (oo instanceof Boolean) 
+				 return ((Boolean)oo).booleanValue(); 
+			return "Y".equals(oo);
+		}
+		return false;
+	}
+
+	/** Set Process Now.
+		@param Processing Process Now	  */
+	public void setProcessing (boolean Processing)
+	{
+		set_Value (COLUMNNAME_Processing, Boolean.valueOf(Processing));
+	}
+
+	/** Get Process Now.
+		@return Process Now	  */
+	public boolean isProcessing () 
+	{
+		Object oo = get_Value(COLUMNNAME_Processing);
+		if (oo != null) 
+		{
+			 if (oo instanceof Boolean) 
+				 return ((Boolean)oo).booleanValue(); 
+			return "Y".equals(oo);
+		}
+		return false;
+	}
+
+	/** Set Search Key.
+		@param Value 
+		Search key for the record in the format required - must be unique
+	  */
+	public void setValue (String Value)
+	{
+		set_Value (COLUMNNAME_Value, Value);
+	}
+
+	/** Get Search Key.
+		@return Search key for the record in the format required - must be unique
+	  */
+	public String getValue () 
+	{
+		return (String)get_Value(COLUMNNAME_Value);
+	}
+}
---
+++ org.globalqss.idempiere.LEC.electronicinvoice/src/org/globalqss/process/ImportSriAccessCode.java
@@ -0,0 +1,204 @@
+/**********************************************************************
+* This file is part of Adempiere ERP Bazaar                           *
+* http://www.adempiere.org                                            *
+*                                                                     *
+* Copyright (C) Contributors                                          *
+*                                                                     *
+* This program is free software; you can redistribute it and/or       *
+* modify it under the terms of the GNU General Public License         *
+* as published by the Free Software Foundation; either version 2      *
+* of the License, or (at your option) any later version.              *
+*                                                                     *
+* This program is distributed in the hope that it will be useful,     *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        *
+* GNU General Public License for more details.                        *
+*                                                                     *
+* You should have received a copy of the GNU General Public License   *
+* along with this program; if not, write to the Free Software         *
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,          *
+* MA 02110-1301, USA.                                                 *
+*                                                                     *
+* Contributors:                                                       *
+* - Jesus Garcia - GlobalQSS Colombia                                 *
+* - Carlos Ruiz  - GlobalQSS Colombia                                 *
+**********************************************************************/
+package org.globalqss.process;
+
+import java.math.BigDecimal;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+
+import org.compiere.model.X_I_SRI_AccessCode;
+import org.compiere.model.X_SRI_AccessCode;
+import org.compiere.process.ProcessInfoParameter;
+import org.compiere.process.SvrProcess;
+import org.compiere.util.DB;
+
+/**
+ *	Import SRI Access Codes from I_SRI_AccessCode
+ *
+ * 	@author 	GlobalQSS/jjgq
+ */
+public class ImportSriAccessCode extends SvrProcess
+{
+	/**	Client to be imported to		*/
+	private int				m_AD_Client_ID = 0;
+	
+	/**	Delete old Imported				*/
+	private boolean			m_deleteOldImported = false;
+	/**	Environment to be imported to		*/
+	private String			m_SRI_ShortDocType = null;
+	/**
+	 *  Prepare - e.g., get Parameters.
+	 */
+	protected void prepare()
+	{
+		ProcessInfoParameter[] para = getParameter();
+		for (int i = 0; i < para.length; i++)
+		{
+			String name = para[i].getParameterName();
+			if (name.equals("SRI_ShortDocType"))
+				m_SRI_ShortDocType = para[i].getParameter().toString();
+			else if (name.equals("DeleteOldImported"))
+				m_deleteOldImported = "Y".equals(para[i].getParameter());
+			else
+				log.log(Level.SEVERE, "Unknown Parameter: " + name);
+		}
+		m_AD_Client_ID = getAD_Client_ID();
+	}	//	prepare
+
+
+	/**
+	 *  Perform process.
+	 *  @return Message
+	 *  @throws Exception
+	 */
+	protected String doIt() throws Exception
+	{
+		StringBuffer sql = null;
+		int no = 0;
+		String clientCheck = " AND AD_Client_ID=" + m_AD_Client_ID;
+		
+		//	****	Prepare	****
+
+		//	Delete Old Imported
+		if (m_deleteOldImported)
+		{
+			sql = new StringBuffer ("DELETE I_SRI_AccessCode "
+				+ "WHERE I_IsImported='Y'").append(clientCheck);
+			no = DB.executeUpdate(sql.toString(), get_TrxName());
+			log.info("Delete Old Imported =" + no);
+		}
+
+		//	Set Client, Org, IsActive, Created/Updated
+		sql = new StringBuffer ("UPDATE I_SRI_AccessCode "
+			+ "SET AD_Client_ID = COALESCE (AD_Client_ID, ").append(m_AD_Client_ID).append("),"
+			+ " AD_Org_ID = COALESCE (AD_Org_ID, 0),"	// TODO Reviewme
+			+ " IsActive = COALESCE (IsActive, 'Y'),"
+			+ " Created = COALESCE (Created, SysDate),"
+			+ " CreatedBy = COALESCE (CreatedBy, 0),"
+			+ " Updated = COALESCE (Updated, SysDate),"
+			+ " UpdatedBy = COALESCE (UpdatedBy, 0),"
+			+ " I_ErrorMsg = ' ',"
+			+ " I_IsImported = 'N' "
+			+ "WHERE I_IsImported<>'Y' OR I_IsImported IS NULL");
+		no = DB.executeUpdate(sql.toString(), get_TrxName());
+		log.info("Reset=" + no);
+		
+		//	Mandatories
+		sql = new StringBuffer ("UPDATE I_SRI_AccessCode "
+			+ "SET I_IsImported='E', I_ErrorMsg=I_ErrorMsg||'ERR=No Mandatory Value,' "
+			+ "WHERE Value IS NULL"
+			+ " AND I_IsImported<>'Y'").append(clientCheck);
+		no = DB.executeUpdate(sql.toString(), get_TrxName());
+		if (no != 0)
+			log.warning("No Mandatory Value=" + no);
+		
+		//	Environment
+		sql = new StringBuffer ("UPDATE I_SRI_AccessCode "
+			+ "SET I_IsImported='E', I_ErrorMsg=I_ErrorMsg||'ERR=Source File Environment differs' "
+			//+ "WHERE Value IS NOT NULL AND SUBSTR(Value,14,1) <> ").append(m_EnvType).append("::bpchar"
+			+ "WHERE Value IS NOT NULL AND SUBSTR(Value,14,1) NOT IN ('1', '2')"
+			+ " AND I_IsImported<>'Y'").append(clientCheck);
+		no = DB.executeUpdate(sql.toString(), get_TrxName());
+		if (no != 0)
+			log.warning("Source File Environment differs=" + no);
+		
+		commitEx();
+		
+		//Import Access Code
+		sql = new StringBuffer ("SELECT * FROM I_SRI_AccessCode WHERE I_IsImported='N'")
+			.append(clientCheck);
+		
+		int noInsert = 0;
+		PreparedStatement pstmt_setImported = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try
+		{
+			pstmt = DB.prepareStatement(sql.toString(), get_TrxName());
+			rs = pstmt.executeQuery();
+			
+			while (rs.next())
+			{
+				X_I_SRI_AccessCode imp = new X_I_SRI_AccessCode(getCtx(), rs, get_TrxName());
+				
+				// New Access Code
+				X_SRI_AccessCode sri_accesscode = new X_SRI_AccessCode (getCtx(), 0, get_TrxName());
+				//sri_accesscode.setAD_ClientID(imp.getAD_Client_ID());
+				sri_accesscode.setAD_Org_ID(imp.getAD_Org_ID());
+				sri_accesscode.setValue(imp.getValue());
+				sri_accesscode.setOldValue(imp.getValue());
+				sri_accesscode.setEnvType("1");	// Pruebas // Before Save ?
+				sri_accesscode.setCodeAccessType("2"); // Contingencia Before Save ?
+				sri_accesscode.setSRI_ShortDocType(m_SRI_ShortDocType);
+				sri_accesscode.setIsUsed(false);
+				
+				// Save Access Code
+				if (sri_accesscode.save())
+				{
+					imp.setI_IsImported(true);
+					imp.setProcessed(true);
+					imp.saveEx();
+					noInsert++;
+
+				}
+			}
+			
+			//		Close database connection
+			rs.close();
+			pstmt.close();
+			rs = null;
+			pstmt = null;
+			
+		}
+
+		catch (SQLException e)
+		{
+			throw e;
+		}
+		finally
+		{
+			DB.close(rs, pstmt);
+			rs = null;
+			pstmt = null;
+			DB.close(pstmt_setImported);
+			pstmt_setImported = null;
+		}
+
+		//	Set Error to indicator to not imported
+		sql = new StringBuffer ("UPDATE I_SRI_AccessCode "
+			+ "SET I_IsImported='N', Updated=SysDate "
+			+ "WHERE I_IsImported<>'Y'").append(clientCheck);
+		no = DB.executeUpdate(sql.toString(), get_TrxName());
+		addLog (0, null, new BigDecimal (no), "@Errors@");
+		//
+		addLog (0, null, new BigDecimal (noInsert), "@SRI_AccessCode_ID@: @Inserted@");
+		return "";
+	}	//	doIt
+	
+
+}	//	ImportSriAccessCode
