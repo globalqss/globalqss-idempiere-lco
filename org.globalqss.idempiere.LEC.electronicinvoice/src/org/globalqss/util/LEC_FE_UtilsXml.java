2cbfacd6455c186ba939c1819feebe62a23ce625
j2garcia <j2garcia@localhost>
2014-05-27 12:40:49 -0500

    [#2145] Adempiere361:Facturaci?n Electr?nica -- Avance desarrollo

---
+++ org.globalqss.idempiere.LEC.electronicinvoice/src/org/globalqss/util/LEC_FE_UtilsXml.java
@@ -0,0 +1,51 @@
+package org.globalqss.util;
+
+
+import org.globalqss.model.GenericXMLSignature;
+import org.w3c.dom.Document;
+
+import es.mityc.firmaJava.libreria.xades.DataToSign;
+import es.mityc.firmaJava.libreria.xades.XAdESSchemas;
+import es.mityc.javasign.EnumFormatoFirma;
+import es.mityc.javasign.xml.refs.AllXMLToSign;
+import es.mityc.javasign.xml.refs.ObjectToSign;
+
+
+/**
+ *	Utils for LEC FE Xml
+ *
+ *  @author Jesus Garcia - globalqss - Quality Systems & Solutions - http://globalqss.com
+ *	@version $Id: LEC_FE_UtilsXml.java,v 1.0 2013/05/27 23:01:26 cruiz Exp $
+ */
+
+public class LEC_FE_UtilsXml extends GenericXMLSignature
+{
+	
+	String SIGN_FILE_NAME = PKCS12_Resource;
+	
+	public void main(String[] args) {
+		LEC_FE_UtilsXml signature = new LEC_FE_UtilsXml();
+        signature.execute();
+
+    }
+	
+    @Override
+    public DataToSign createDataToSign() {
+        DataToSign dataToSign = new DataToSign();
+        dataToSign.setXadesFormat(EnumFormatoFirma.XAdES_BES);
+        dataToSign.setEsquema(XAdESSchemas.XAdES_132);
+        dataToSign.setXMLEncoding(XmlEncoding);
+        dataToSign.setEnveloped(true);
+        Document docToSign = getDocument(PKCS12_Resource);
+        dataToSign.setDocument(docToSign);
+        dataToSign.addObject(new ObjectToSign(new AllXMLToSign(), "Documento de ejemplo", null, "text/xml", null));
+        return dataToSign;
+    }
+    
+    @Override
+    public String getSignatureFileName() {
+        return SIGN_FILE_NAME;
+    }
+
+	
+}	// LEC_FE_UtilsXml
